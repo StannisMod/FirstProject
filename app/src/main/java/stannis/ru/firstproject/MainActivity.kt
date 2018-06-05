56eec2de2c2911088c44a84e23dd29193d772eb0
StannisMod <stas.batalenkov@mail.ru>
2018-06-05 14:59:04 +0300

    Added general container with buttons and stats panel. Starting including assets

--- app/src/main/java/stannis/ru/firstproject/MainActivity.kt
+++ app/src/main/java/stannis/ru/firstproject/MainActivity.kt
@@ -2,11 +2,33 @@ package stannis.ru.firstproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.stats_panel.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mail.setOnClickListener {
+            // startActivity(mail)
+        }
+
+        toinventory.setOnClickListener {
+            // startActivity(inventory)
+        }
+
+        tomarket.setOnClickListener {
+            // startActivity(market)
+        }
+
+        stats_panel.setOnClickListener {
+            // startActivity(stats)
+        }
+
+        tofactory.setOnClickListener {
+            // startActivity(factory)
+        }
     }
 }
